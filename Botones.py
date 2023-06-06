0163717f57aba26f1694263c5820f73aa8dbad71
IsraelSotoBustamante <135078026+IsraelSotoBustamante@users.noreply.github.com>
2023-06-06 15:31:50 -0600

    Documentacion Botones.py

--- Botones.py
+++ Botones.py
@@ -1,16 +1,20 @@
 "Practica 1,Botones"
+
+"Se llama a la biblioteca y las herramientas a utilizar"
 from tkinter import Button, Label, Tk
 
-
+"Se crea la ventana y se le asigna un nombre"
 class VentanaEjemplo:
     def __init__(self, master):
         self.master = master
 
+        "Texto de la parte superior de la interfaz"
         master.title("Una simple interfaz gráfica")
 
         self.etiqueta = Label(master, text="Esta es la primera ventana!")
         self.etiqueta.pack()
 
+        "Botones, se declaran los objetos, se les asigna un texto y un comando a ejecutar"
         self.botonSaludo = Button(master, text="Saludar", command=self.saludar)
         self.botonSaludo.pack()
 
